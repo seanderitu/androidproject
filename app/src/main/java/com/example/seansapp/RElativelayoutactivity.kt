81b7c89fdc78181199a8c656ba593b2706a7fd46
seanderitu <seangithaiga@gmail.com>
2023-03-31 13:03:53 +0300

    worked on layouts

---
+++ app/src/main/java/com/example/seansapp/RElativelayoutactivity.kt
@@ -0,0 +1,11 @@
+package com.example.seansapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RElativelayoutactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relativelayoutactivity)
+    }
+}
