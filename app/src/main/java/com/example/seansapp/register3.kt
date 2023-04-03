50f2670835b764e1a4853612c0286cea72d61178
seanderitu <seangithaiga@gmail.com>
2023-04-03 12:02:35 +0300

    register activity

---
+++ app/src/main/java/com/example/seansapp/register3.kt
@@ -0,0 +1,11 @@
+package com.example.seansapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class register3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register3)
+    }
+}
